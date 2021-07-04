c234990a16cea043c4335b0de1e4284a7609935c
John Doe <johndoe@example.com>
2021-07-05 01:29:20 +0800

    docs(*): add 'error case' message

--- 0001-Two-Sum/solution.js
+++ 0001-Two-Sum/solution.js
@@ -7,6 +7,7 @@ var twoSum = function (nums, target) {
   if (nums.length == 2) {
     return [0, 1];
   }
+
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] === target - nums[j]) {
@@ -14,6 +15,8 @@ var twoSum = function (nums, target) {
       }
     }
   }
+
+  return [-1, -1];
 };
 
 /**

786933dfe0d6303b03218fb7dbd71c5d5b0486d6
John Doe <johndoe@example.com>
2023-09-20 08:47:44 -0400

    style(*): format code

--- 1337-the-k-weakest-rows-in-a-matrix/solution.js
+++ 1337-the-k-weakest-rows-in-a-matrix/solution.js
@@ -1,30 +1,24 @@
-"use strict";
-
 /**
  * @param {number[][]} mat
  * @param {number} k
  * @return {number[]}
  */
-var kWeakestRows = function (mat, k) {
-  let matSum = [];
+const kWeakestRows = function (mat, k) {
+  const matSum = [];
 
-  for (let i = 0; i < mat.length; i++) {
-    const rowSum = mat[i].reduce(function (x, y) {
-      return x + y;
-    }, 0);
+  for (let i = 0; i < mat.length; i += 1) {
+    const rowSum = mat[i].reduce((x, y) => x + y, 0);
     matSum.push([rowSum, i]);
   }
 
-  matSum.sort(function (a, b) {
+  matSum.sort((a, b) => {
     if (a[0] === b[0]) {
       return a[1] - b[1];
     }
     return a[0] - b[0];
   });
 
-  const result = matSum.slice(0, k).map(function (number) {
-    return number[1];
-  });
+  const result = matSum.slice(0, k).map((number) => number[1]);
 
   return result;
 };
